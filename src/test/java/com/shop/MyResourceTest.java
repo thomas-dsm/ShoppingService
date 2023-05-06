44dc712f39c6742fbf5af3599baee6e9355a1175
Da Silva Mendonca Thomas <thomas.dasilvamendonca@protonmail.com>
2023-05-06 17:13:56 +0200

    initial commit

---
+++ src/test/java/com/shop/MyResourceTest.java
@@ -0,0 +1,29 @@
+package com.shop;
+
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.shop.MyResource;
+
+public class MyResourceTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(MyResource.class);
+    }
+
+    /**
+     * Test to see that the message "Got it!" is sent in the response.
+     */
+    @Test
+    public void testGetIt() {
+        final String responseMsg = target().path("myresource").request().get(String.class);
+
+        assertEquals("Hello, Heroku!", responseMsg);
+    }
+}
